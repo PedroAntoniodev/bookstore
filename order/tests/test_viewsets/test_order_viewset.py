fcbe9e618e4b6c1e91c7ab8c98820f57faef1b92
PedroAntoniodev <pedrinhox333@gmail.com>
2025-11-18 11:00:16 -0300

    adicionando token de autenticação no order

--- order/tests/test_viewsets/test_order_viewset.py
+++ order/tests/test_viewsets/test_order_viewset.py
@@ -2,6 +2,7 @@ import json
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
+from rest_framework.authtoken.models import Token
 
 from product.factories import CategoryFactory, ProductFactory
 from order.factories import UserFactory, OrderFactory
@@ -12,6 +13,11 @@ class TestOrderViewSet(APITestCase):
 
     def setUp(self):
         self.client = APIClient() 
+
+        self.user = UserFactory()
+        token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
         self.category = CategoryFactory(title="technology")
         self.product = ProductFactory(title="mouse", price=100)
         self.product.category.set([self.category])
@@ -32,11 +38,10 @@ class TestOrderViewSet(APITestCase):
         self.assertEqual(order_data["products"][0]["category"][0]["title"], self.category.title)
 
     def test_create_order(self):
-        user = UserFactory()
         product = ProductFactory()
         data = json.dumps({
             "products_id": [product.id],
-            "user": user.id
+            "user": self.user.id
         })
 
         response = self.client.post(
@@ -50,6 +55,6 @@ class TestOrderViewSet(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        created_order = Order.objects.get(user=user)
-        self.assertEqual(created_order.user, user)
+        created_order = Order.objects.get(user=self.user)
+        self.assertEqual(created_order.user, self.user)
         self.assertIn(product, created_order.products.all())
